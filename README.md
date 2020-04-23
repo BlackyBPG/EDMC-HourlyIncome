5ee4081c9b5f311a598fdd6a4bf133bc4847a68d
Blacky_BPG <wiki@blackpanthergroup.de>
2020-04-23 18:33:33 +0200

    fork release Version 20.04.18_b1920
    first release from original fork

--- README.md
+++ README.md
@@ -1,11 +1,48 @@
 # Hourly Income EDMC Plugin
 
-This is a simple plugin for [EDMarketConnector](https://github.com/Marginal/EDMarketConnector/wiki), based on and adapted from the [EDMCJumpSpeed](https://github.com/inorton/EDMCJumpSpeed) plugin, so credit for most of the work done should be directed to their makers!
+Das ist ein einfaches Plugin für den [ED MarketConnector](https://github.com/Marginal/EDMarketConnector/wiki), basierend aufdem Original [EDMC-HourlyIncome](https://github.com/Exynom/EDMC-HourlyIncome) vom Exynom!
 
-![In-game Screenshot](Screenshot.png)
+![In-game Screenshot](edmc_plugins_hi.png) ![In-game Screenshot](edmc_plugins_light.png) ![In-game Screenshot](edmc_plugins_dark.png)
 
-As with other EDMC plugins, simply unzip the git directory into your plugins folder (or just place load.py inside a directory within the plugins directory) yielding something like
+
+## Installation
+
+So wie auch alle anderen EDMC-Plugins wird der Ordner aus dem heruntergeladenen Archiv in den Plugin-Ordner eures EDMC's entpackt, das sollte danach dann in etwa so aussehen:
 ```
-EDMarketConnector\plugins\EDMC-HourlyIncome-master\load.py
+$AppPath$\EDMarketConnector\plugins\HourlyIncome
 ```
-and restart EDMC afterwards.
+Nach dem starten des EDMC ist das Plugin sofort einsatzbereit, es ist bereits kompatibel mit der neuen BETA-Version des EDMC 3.50 beta0, funktioniert jedoch auch in der Version 3.43 des EDMC.
+
+
+## Anzeigen
+
+Es wird folgendes im Plugin angezeigt:
+
+- Zeile 1:
+- - Links: Andockrate pro Stunde dieser Sitzung
+- - Rechts: Andockrate pro Stunde gesamte Spielzeit
+- Zeile 2:
+- - Links: Durchschnittsverdienst pro Stunde dieser Sitzung
+- - Rechts: Durchschnittsverdienst pro Stunde gesamt Spielzeit
+- Zeile 3:
+- - komplettes Einkommen dieser Sitzung
+- Zeile 4:
+- - aktueller Bar-Besitz (so wie er auch im Spiel angezeigt wird)
+
+Negative Zahlen werden zusätzlich in ROT dargestellt.
+Die Zeit die ein Profil gespielt wurde sowie der aktuelle Bar-Besitz wird beim Laden des Spieles (also EDMC vorher starten) abgefragt.
+Die Gesamtanzahl der Andockvorgänge wird erst mitgeloggt ab dem ersten benutzen des Plugins, dieser Wert wird zwischengespeichert.
+Der Durchschnittsverdienst der gesamten Spielzeit wird automatisch errechnet anhand des aktuellen bar-Besitzes sowie der gesamten Spielzeit, die Werte welche für die Sitzung gültig sind werden eben nur für die Einnahmen und Ausgaben auf die Spielzeit der Sitzung berechnet. Selbige Berechnung gilt für die Andockvorgänge.
+
+
+## Wichtiges
+
+Das Plugin erkennt nicht automatisch welches Design (Theme) man in EDMC aktiviert hat, weshalb es eine Optionsseite mit der möglichkeit der Designwahl für das Plugin gibt.
+
+![EDMC Optionen](edmc_options_hourlyincome.png)
+
+
+## Weiteres
+
+Dieses Plugin ist lediglich für eigene statistische Auswertungen gedacht und synchronisiert sich selbst in keinster Weise mit irgendwelchen anderen Plattformen.
+Für jene welche in anderen Sprachen spielen ist es möglich weitere Übersetzungsdateien an zu legen, diese kommen dann ebenso wie die deutsche Übersetzung in den L10n Ordner innerhalb des Plugin-Ordners.
--- load.py
+++ load.py
@@ -1,9 +1,17 @@
 """
 The "Hourly Income" Plugin
+fork from original by Blacky_BPG
 """
-try: #py3
+
+import l10n
+import functools
+import myNotebook as nb
+
+_ = functools.partial(l10n.Translations.translate, context=__file__)
+
+try:
     import tkinter as tk
-except: #py2
+except ImportError:
     import Tkinter as tk
 import sys
 import time
@@ -16,7 +24,14 @@ try:
 except ImportError:
     config = dict()
 
+APP_VERSION = "20.04.18_b1920"
+
 CFG_EARNINGS = "EarningSpeed_earnings"
+CFG_DOCKINGS = "EarningSpeed_dockings"
+CFG_TIME = "EarningSpeed_time"
+CFG_DESIGN = "EarningSpeed_design"
+COLOR_RED = ("#AD1616", "#FF4040")
+COLOR_NORM = ("#000000", "#80FFFF")
 
 
 class Transaction(object):
@@ -35,43 +50,78 @@ class HourlyIncome(object):
     speed_widget = None
     rate_widget = None
     earned_widget = None
+    single_widget = None
+    att_loockup = 0
     saved_earnings = 0
+    saved_docking = 0
+    saved_time = 0
+    start_time = 0
+    appdesign = 0
     transactions = []
 
     def reset(self):
         """
         Reset button pressed
-        :return:
+        !! disabled for better visuals in app
         """
         self.transactions = []
+        self.att_loockup = 0
         self.saved_earnings = 0
+        self.saved_docking = 0
+        self.saved_time = 0
+        self.start_time = 0
         self.update_window()
         self.save()
 
     def load(self):
         """
-        Load saved earnings from config
-        :return:
+        Load saved earnings, saved dockings and saved gametime from config
         """
+        if config.get(CFG_DESIGN):
+            self.appdesign = int(config.get(CFG_DESIGN))
+        else:
+           self.appdesign = 0
+
         saved = config.get(CFG_EARNINGS)
         if not saved:
             self.saved_earnings = 0.0
         else:
             self.saved_earnings = float(saved)
 
+        savedD = config.get(CFG_DOCKINGS)
+        if not savedD:
+            self.saved_docking = 0.0
+        else:
+            self.saved_docking = float(savedD)
+
+        savedT = config.get(CFG_TIME)
+        if not savedT:
+            self.saved_time = 0.0
+        else:
+            self.saved_time = float(savedT)
+
     def save(self):
         """
         Save the saved earnings to config
-        :return:
         """
         config.set(CFG_EARNINGS, str(self.saved_earnings + self.trip_earnings()))
+        config.set(CFG_DOCKINGS, str(self.saved_docking + self.dockings()))
+        config.set(CFG_TIME, str(self.saved_time + self.sincetime()))
+
+    def start_data(self, totaltime):
+        """
+        """
+        self.saved_time = totaltime / 3600
+        self.update_window()
+        self.save()
 
     def transaction(self, earnings):
         """
         Record a transaction
-        :param earnings:
-        :return:
         """
+        if self.start_time == 0:
+            self.start_time = time.time()
+
         data = Transaction()
         data.earnings = earnings
         data.is_docking_event = 0.0
@@ -82,55 +132,110 @@ class HourlyIncome(object):
 
     def register_docking(self):
         """
-        Record a transaction
-        :param earnings:
-        :return:
+        Register on docked state
         """
-        data = Transaction()
-        data.earnings = 0.0
-        data.is_docking_event = 1.0
-        data.time = time.time()
-        self.transactions.append(data)
+        if self.start_time == 0:
+            self.start_time = time.time()
+
+        if self.att_loockup > 0:
+            self.update_window()
+            self.save()
+        else:
+            data = Transaction()
+            data.earnings = 0.0
+            data.is_docking_event = 1.0
+            data.time = time.time()
+            self.transactions.append(data)
+            self.update_window()
+            self.save()
+
+    def starttime(self):
+        """
+        start timer for daily statistics
+        """
+        self.start_time = time.time()
         self.update_window()
-        self.save()
+
+    def loockup(self,onoff):
+        """
+        dont log first docking on game load with this function
+        """
+        self.att_loockup = onoff
+        
+    def sincetime(self):
+        """
+        """
+        if self.start_time > 0:
+            return (time.time() - self.start_time) / 3600
+        else:
+            return 1
 
     def trip_earnings(self):
         """
         Measure how much we've earned
-        :return:
+        :return sum of earnings for trip:
         """
         return sum([x.earnings for x in self.transactions])
+
+    def dockings(self):
+        """
+        Get the station visits rate
+        :return dockings for this trip:
+        """
+        if len(self.transactions) > 1:
+            return sum([x.is_docking_event for x in self.transactions])
+        else:
+            return 0
 
     def rate(self):
         """
         Get the station visits/hr rate
-        :return:
+        :return dockings overall per hour:
         """
         if len(self.transactions) > 1:
-            started = self.transactions[0].time
-            now = time.time()
-            return sum([x.is_docking_event for x in self.transactions]) * 60.0 * 60.0 / (now - started)
+            return (self.saved_docking + self.dockings()) / (self.saved_time + self.sincetime())
+        elif self.saved_docking > 0:
+            return self.saved_docking / self.saved_time
+        else:
+            return 0
+
+    def ratenow(self):
+        """
+        Get the station visits/hr rate
+        :return dockings for this trip per hour:
+        """
+        if len(self.transactions) > 1:
+            return self.dockings() / self.sincetime()
         else:
             return 0.0
 
     def speed(self):
         """
         Get the earning speed in Cr/hr
-        :return:
+        :return earnings overall per hour:
         """
-        earned = self.trip_earnings()
         if len(self.transactions) > 1:
-            started = self.transactions[0].time
-            now = time.time()
-            return earned * 60.0 * 60.0 / (now - started)
+            return (self.saved_earnings + self.trip_earnings()) / (self.saved_time + self.sincetime())
+        elif self.saved_earnings > 1:
+            return self.saved_earnings / self.saved_time
+        else:
+            return 0
+
+    def speednow(self):
+        """
+        Get the earning speed in Cr/hr
+        :return earnings for trip per hour:
+        """
+        if len(self.transactions) > 1:
+            return self.trip_earnings() / self.sincetime()
         else:
             return 0.0
 
     def update_window(self):
         """
         Update the EDMC window
-        :return:
         """
+        self.update_single()
         self.update_earned()
         self.update_transaction_rate()
         self.update_hourlyincome()
@@ -138,42 +243,88 @@ class HourlyIncome(object):
     def update_transaction_rate(self):
         """
         Set the transaction rate rate in the EDMC window
-        :param msg:
-        :return:
         """
-        msg = "{} Visits/hr".format(Locale.stringFromNumber(self.rate(), 2))
+        msg = " {}".format(Locale.stringFromNumber(self.rate(), 2))
+        self.rate_widget["foreground"] = COLOR_NORM[self.appdesign]
         self.rate_widget.after(0, self.rate_widget.config, {"text": msg})
+
+        msgnow = "  {}  |".format(Locale.stringFromNumber(self.ratenow(), 2))
+        self.ratenow_widget["foreground"] = COLOR_NORM[self.appdesign]
+        self.ratenow_widget.after(0, self.ratenow_widget.config, {"text": msgnow})
 
     def update_hourlyincome(self):
         """
         Set the transaction speed rate in the EDMC window
-        :param msg:
-        :return:
         """
-        msg = "{} Cr/hr".format(Locale.stringFromNumber(self.speed(), 2))
+        msg = " {}".format(Locale.stringFromNumber(self.speed(), 2))
+        if self.speed() > 0:
+            self.speed_widget["foreground"] = COLOR_NORM[self.appdesign]
+        else:
+            self.speed_widget["foreground"] = COLOR_RED[self.appdesign]
+
+        self.speed_widget.grid(row=1, column=2, sticky=tk.E)
         self.speed_widget.after(0, self.speed_widget.config, {"text": msg})
+
+        msgnow = "  {}  |".format(Locale.stringFromNumber(self.speednow(), 2))
+        if self.speednow() > 0:
+            self.speednow_widget["foreground"] = COLOR_NORM[self.appdesign]
+        else:
+            self.speednow_widget["foreground"] = COLOR_RED[self.appdesign]
+
+        self.speednow_widget.grid(row=1, column=1, sticky=tk.E)
+        self.speednow_widget.after(0, self.speednow_widget.config, {"text": msgnow})
 
     def update_earned(self):
         """
-        Set the transaction speed rate in the EDMC window
-        :param msg:
-        :return:
+        Set the transaction sum in the EDMC window
         """
-        msg = "{} Cr".format(Locale.stringFromNumber(self.trip_earnings() + self.saved_earnings, 2))
+        msg = "  {}".format(Locale.stringFromNumber(self.trip_earnings() + self.saved_earnings, 0))
+        self.earned_widget["foreground"] = COLOR_NORM[self.appdesign]
         self.earned_widget.after(0, self.earned_widget.config, {"text": msg})
 
+    def update_single(self):
+        """
+        Set the transaction speed rate for trip in the EDMC window
+        """
+        msg = "{}".format(Locale.stringFromNumber(self.trip_earnings(), 0))
+        if self.trip_earnings() > 0:
+            self.single_widget["foreground"] = COLOR_NORM[self.appdesign]
+        else:
+            self.single_widget["foreground"] = COLOR_RED[self.appdesign]
 
-def plugin_start():
-    hourlyincome = HourlyIncome()
-    hourlyincome.load()
-    this.hourlyincome = hourlyincome
-    # this.hourlyincome.transaction(0)
+        self.single_widget.grid(row=2, column=1, sticky=tk.E, columnspan=2)
+        self.single_widget.after(0, self.single_widget.config, {"text": msg})
+
+
+
+def plugin_prefs(parent, cmdr, is_beta):
+    if config.get(CFG_DESIGN) != None:
+        this.appdesign = tk.IntVar(value=config.get(CFG_DESIGN))
+    else:
+        this.appdesign = tk.IntVar(value=0)
+
+    frame = nb.Frame(parent)
+    nb.Label(frame, text="HourlyIncome-EDMC Version: {INSTALLED}\n".format(INSTALLED=APP_VERSION)).grid(padx=10, sticky=tk.W)
+    nb.Checkbutton(frame, text=_("Dark Theme").encode('utf-8'), variable=this.appdesign, onvalue = 1, offvalue = 0).grid(padx=10, pady = 3, sticky=tk.W)
+    return frame
+
+
+def prefs_changed(cmdr, is_beta):
+    hourlyincome = this.hourlyincome
+    hourlyincome.appdesign = int(this.appdesign.get())
+    config.set(CFG_DESIGN, str(hourlyincome.appdesign))
+    hourlyincome.update_window()
+
 
 def plugin_start3(plugin_dir):
     hourlyincome = HourlyIncome()
     hourlyincome.load()
     this.hourlyincome = hourlyincome
-    # this.hourlyincome.transaction(0)
+
+def plugin_start():
+    hourlyincome = HourlyIncome()
+    hourlyincome.load()
+    this.hourlyincome = hourlyincome
 
 def plugin_app(parent):
     """
@@ -183,38 +334,59 @@ def plugin_app(parent):
 
     frame = tk.Frame(parent)
 
-    hourlyincome.rate_widget = tk.Label(
-        frame,
-        text="...",
-        justify=tk.RIGHT)
-    rate_label = tk.Label(frame, text="Station Visit Rate:", justify=tk.LEFT)
-    rate_label.grid(row=0, column=0, sticky=tk.W)
+    hourlyincome.rate_widget = tk.Label(frame, text="", justify=tk.RIGHT)
     hourlyincome.rate_widget.grid(row=0, column=2, sticky=tk.E)
+    rate_label = tk.Label(frame, text=_("Dock rate:").encode('utf-8'), justify=tk.LEFT)
+    rate_label.grid(row=0, column=0, sticky=tk.W)
+    rateT_label = tk.Label(frame, text=_("p.Hr").encode('utf-8'), justify=tk.LEFT)
+    rateT_label.grid(row=0, column=4, sticky=tk.W)
 
-    hourlyincome.speed_widget = tk.Label(
-        frame,
-        text="...",
-        justify=tk.RIGHT)
-    speed_label = tk.Label(frame, text="Hourly Income:", justify=tk.LEFT)
-    speed_label.grid(row=1, column=0, sticky=tk.W)
+    hourlyincome.ratenow_widget = tk.Label(frame, text="", justify=tk.RIGHT)
+    hourlyincome.ratenow_widget.grid(row=0, column=1, sticky=tk.E)
+
+    hourlyincome.speed_widget = tk.Label(frame, text="", justify=tk.RIGHT)
     hourlyincome.speed_widget.grid(row=1, column=2, sticky=tk.E)
+    speed_label = tk.Label(frame, text=_("Average:").encode('utf-8'), justify=tk.LEFT)
+    speed_label.grid(row=1, column=0, sticky=tk.W)
+    speedT_label = tk.Label(frame, text=_("Cr/hr").encode('utf-8'), justify=tk.LEFT)
+    speedT_label.grid(row=1, column=4, sticky=tk.W)
 
-    hourlyincome.earned_widget = tk.Label(
-        frame,
-        text="...",
-        justify=tk.RIGHT)
-    earned_label = tk.Label(frame, text="Total Income:", justify=tk.LEFT)
-    earned_label.grid(row=2, column=0, sticky=tk.W)
-    hourlyincome.earned_widget.grid(row=2, column=2, sticky=tk.E)
+    hourlyincome.speednow_widget = tk.Label(frame, text="", justify=tk.RIGHT)
+    hourlyincome.speednow_widget.grid(row=1, column=1, sticky=tk.E)
 
+    hourlyincome.single_widget = tk.Label(frame, text="", justify=tk.RIGHT)
+    hourlyincome.single_widget.grid(row=2, column=1, sticky=tk.E, columnspan=2)
+    single_label = tk.Label(frame, text=_("Earnings:").encode('utf-8'), justify=tk.LEFT)
+    single_label.grid(row=2, column=0, sticky=tk.W)
+    singleT_label = tk.Label(frame, text="Cr", justify=tk.LEFT)
+    singleT_label.grid(row=2, column=4, sticky=tk.W)
+
+    hourlyincome.earned_widget = tk.Label(frame, text="", justify=tk.RIGHT)
+    hourlyincome.earned_widget.grid(row=3, column=1, sticky=tk.E, columnspan=2)
+    earned_label = tk.Label(frame, text=_("Cash holding:").encode('utf-8'), justify=tk.LEFT)
+    earned_label.grid(row=3, column=0, sticky=tk.W)
+    earnedT_label = tk.Label(frame, text="Cr", justify=tk.LEFT)
+    earnedT_label.grid(row=3, column=4, sticky=tk.W)
+
+    """
     reset_btn = tk.Button(frame, text="Reset", command=hourlyincome.reset)
-    reset_btn.grid(row=2, column=1, sticky=tk.W)
+    reset_btn.grid(row=3, column=1, sticky=tk.W)
+    """
 
+    frame.columnconfigure(0, weight=0)
+    frame.columnconfigure(1, weight=1)
     frame.columnconfigure(2, weight=1)
+    frame.columnconfigure(3, weight=0)
+    frame.grid_columnconfigure(1, minsize=95)
+    frame.grid_columnconfigure(2, minsize=95)
 
     this.spacer = tk.Frame(frame)
     hourlyincome.update_window()
     return frame
+
+
+def dashboard_entry(cmdr, is_beta, entry):
+    this.hourlyincome.update_window()
 
 
 def journal_entry(cmdr, is_beta, system, station, entry, state):
@@ -229,7 +401,15 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
     """
     if "event" in entry:
         # ! trading
-        if "MarketSell" in entry["event"]:
+        if "LoadGame" in entry["event"]:
+            this.hourlyincome.saved_earnings = entry["Credits"]
+            this.hourlyincome.starttime()
+            this.hourlyincome.loockup(1)
+        elif "Loadout" in entry["event"]:
+            this.hourlyincome.loockup(0)
+        elif "Statistics" in entry["event"]:
+            this.jumpspeed.start_data(entry["Time_Played"])
+        elif "MarketSell" in entry["event"]:
             this.hourlyincome.transaction(entry["TotalSale"])
         elif "MarketBuy" in entry["event"]:
             this.hourlyincome.transaction(-entry["TotalCost"])
@@ -254,7 +434,7 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
             this.hourlyincome.transaction(-entry["Cost"])
         # ! shipyard/outfitting/engineering
         elif "ModuleBuy" in entry["event"]:
-            this.hourlyincome.transaction(-entry["Buyprice"])
+            this.hourlyincome.transaction(-entry["BuyPrice"])
             if "SellItem" in entry:
                 this.hourlyincome.transaction(entry["SellPrice"])
         elif "ModuleSell" in entry["event"]:
@@ -283,13 +463,6 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
         # ! combat
         elif "RedeemVoucher" in entry["event"]:
             this.hourlyincome.transaction(entry["Amount"])
-        # ? These are probably logged upon award of bond/voucher and not upon payment; Instead, RedeemVoucher is logged upon payment
-        # elif "Bounty" in entry["event"]:
-        #     this.hourlyincome.transaction(entry["TotalReward"])
-        # elif "CapShipBond" in entry["event"]:
-        #     this.hourlyincome.transaction(entry["Reward"])
-        # elif "FactionKillBond" in entry["event"]:
-        #     this.hourlyincome.transaction(entry["Reward"])
         # ! exploration
         elif "BuyExplorationData" in entry["event"]:
             this.hourlyincome.transaction(-entry["Cost"])
@@ -323,3 +496,5 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
         # ! is_docking_event
         elif "Docked" in entry["event"]:
             this.hourlyincome.register_docking()
+
+        this.hourlyincome.update_window()
